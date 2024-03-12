6d923a80bd60a250bba4eff3fb1a3c37c1d5cf40
ky21 <calvin.yang.cs@gmail.com>
2024-03-12 11:41:12 +0000

    update a new welcome page

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -15,65 +15,15 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import ShareIcon from "@mui/icons-material/Share";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import Welcome from "./Welcome";
 
 const Feed = () => {
   return (
     <Box flex={10} p={2}>
-      <Card>
-        <CardHeader
-          avatar={
-            <Avatar sx={{ bgcolor: "skyblue" }} aria-label="recipe">
-              R
-            </Avatar>
-          }
-          action={
-            <IconButton aria-label="settings">
-              <MoreVertIcon />
-            </IconButton>
-          }
-          title="NHS Help"
-        />
-        <CardMedia
-          component="img"
-          height="20%"
-          image="https://dailysceptic.org/wp-content/uploads/2022/12/Rainbow-NHS-badge_Video-01-01-01-01-1024x576-1.png"
-          alt="Paella dish"
-        />
-        <CardContent>
-          <Typography variant="body2" color="text.secondary">
-            This is NHS help webpage, please to login in first to use our
-            service
-          </Typography>
-        </CardContent>
-      </Card>
+      <Welcome />
+      <Welcome />
 
-      <Card>
-        <CardHeader
-          avatar={
-            <Avatar sx={{ bgcolor: "skyblue" }} aria-label="recipe">
-              R
-            </Avatar>
-          }
-          action={
-            <IconButton aria-label="settings">
-              <MoreVertIcon />
-            </IconButton>
-          }
-          title="NHS Help"
-        />
-        <CardMedia
-          component="img"
-          height="20%"
-          image="https://dailysceptic.org/wp-content/uploads/2022/12/Rainbow-NHS-badge_Video-01-01-01-01-1024x576-1.png"
-          alt="Paella dish"
-        />
-        <CardContent>
-          <Typography variant="body2" color="text.secondary">
-            This is NHS help webpage, please to login in first to use our
-            service
-          </Typography>
-        </CardContent>
-      </Card>
+      <Welcome />
     </Box>
   );
 };
